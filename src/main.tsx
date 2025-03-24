212eff812baa293e48be7b7946d343d5e6374279
Rageemon <145965461+Rageemon@users.noreply.github.com>
2025-03-24 18:41:10 +0530

    depl

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 // Create a client
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <Router>
+      <Router basename="/Wordle-Solver">
         <App />
       </Router>
   </StrictMode>,
